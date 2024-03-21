55206d0895cee67f64268f9d445d3f4d09a93f29
federicomaniglio <fmaniglio@itisrossi.vi.it>
2024-03-21 08:38:48 +0100

    promise and future

--- main.cpp
+++ main.cpp
@@ -3,43 +3,29 @@
 #include <mutex>
 #include <condition_variable>
 #include <cstdlib>
+#include <future>
 
 using namespace std;
 
-std::condition_variable cv;
-std::mutex m;
-long saldo = 0;
-
-void aggiungiSoldi(int soldi) {
-    this_thread::sleep_for(chrono::seconds(2));
-    std::lock_guard<mutex> lg(m);
-    saldo += soldi;
-    cout << "Aggiunto " << soldi << " Saldo attuale: " << saldo << endl;
-    cv.notify_one();
+void calcolaPari(promise<long long>& valorePromesso, long long fine){
+    long long numeriPari=0;
+    for (long long i = 0; i < fine; ++i) {
+        if(!(i%2))
+            numeriPari++;
+    }
+    valorePromesso.set_value(numeriPari);
 }
 
-void prelevaSoldi(int soldi) {
-    std::unique_lock<mutex> ul(m);
-    cv.wait(ul, [soldi] {
-        if ((saldo - soldi) >= 0)
-            return true;
-        else {
-            cout << "Non è possibile dedurre l'importo, il saldo attuale è inferiore a " << soldi << endl;
-            return false;
-        }
-    });
-
-    saldo -= soldi;
-    cout << "Importo dedotto: " << soldi << " Il saldo attuale è: " << saldo << endl;
-}
 
 int main() {
-    srand(time(NULL));
-    thread t1(prelevaSoldi, rand() % 500 + 1);
-    thread t2(aggiungiSoldi, rand() % 500 + 1);
 
-    t1.join();
-    t2.join();
+    promise<long long> valorePromesso;
+    future<long long> valoreFuturo = valorePromesso.get_future();
+
+    thread t(calcolaPari, ref(valorePromesso), 9000000000);
+    cout<<"Calcolo in corso..."<<endl;
+    cout<< "Numeri pari: " << valoreFuturo.get()<<endl;
+    t.join();
 
     return 0;
 }
